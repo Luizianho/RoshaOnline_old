5c0f495c038b7a367e8f1d89d1aa65c4a97ddd65
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2023-10-19 13:06:19 +0200

    First push

---
+++ Source/RoshaOnline/RoshaOnlineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoshaOnlineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSHAONLINE_API ARoshaOnlineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
