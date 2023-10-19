3c027a4b634b71809d44865fde28eb50d9db3c9c
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2023-10-19 13:27:34 +0200

    Added game mode for Server

---
+++ Source/RoshaOnline/Public/RoshaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "RoshaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSHAONLINE_API ARoshaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
