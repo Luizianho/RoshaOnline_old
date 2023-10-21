f9a854f17409684b94844dbdbfdd27f03a6c861e
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2023-10-21 16:06:37 +0200

    Added simple player movement

--- Source/RoshaOnline/Private/Movement_AnimInstance.cpp
+++ Source/RoshaOnline/Private/Movement_AnimInstance.cpp
@@ -2,4 +2,38 @@
 
 
 #include "Movement_AnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MMOCharacter.h"
 
+
+void UMovement_AnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+		if (Pawn)
+		{
+			MMOCharacter = Cast<AMMOCharacter>(Pawn);
+		}
+	}
+}
+
+void UMovement_AnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size();
+
+		if (MMOCharacter == nullptr)
+		{
+			MMOCharacter = Cast<AMMOCharacter>(Pawn);
+		}
+	}
+}
--- Source/RoshaOnline/Public/Movement_AnimInstance.h
+++ Source/RoshaOnline/Public/Movement_AnimInstance.h
@@ -13,5 +13,20 @@ UCLASS()
 class ROSHAONLINE_API UMovement_AnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	class APawn* Pawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	class AMMOCharacter* MMOCharacter;
 	
 };
